c816f0a6f14a1282c1819c8e907436f9db8a82a5
Piquesel <piquesel@me.com>
2018-11-18 00:18:37 +0100

    Episode 8 of Coding Math by Keith Peters

--- ep8.py
+++ ep8.py
@@ -1,11 +1,28 @@
 # Coding Math Episode 8
-# 
+#
 __author__ = "piquesel"
 
 import pygame
-import random
 import math
+import random
 from ep7 import Vector as Vector
+
+
+class Particle:
+    'Represents a particle defined by its position, velocity and direction'
+
+    def __init__(self, x, y, speed, direction):
+        'Initialize the particle'
+        self.x = x
+        self.y = y
+        self.position = Vector(x, y)
+        self.velocity = Vector(0, 0)
+        self.velocity.set_length(speed)
+        self.velocity.set_angle(direction)
+
+    def update(self):
+        self.position.add_to(self.velocity)
+
 
 pygame.init()
 
@@ -15,10 +32,14 @@ screen = pygame.display.set_mode((800, 600))
 screen_rect = screen.get_rect()
 pygame.display.set_caption("Episode 8")
 
-position = Vector(100, 100)
-velocity = Vector()
-velocity.set_length(3)
-velocity.set_angle(math.pi / 6)
+particles = []
+NUM_PARTICLES = 300
+
+for i in range(0, NUM_PARTICLES):
+    particles.append(Particle(screen_rect.width/2,
+                              screen_rect.height/2,
+                              random.random() * 4 + 1,
+                              random.random() * math.pi * 2))
 
 main_loop = True
 while main_loop:
@@ -29,18 +50,14 @@ while main_loop:
             or event.type == pygame.KEYDOWN
             and event.key == pygame.K_ESCAPE):
                 main_loop = False
-        if (event.type == pygame.KEYDOWN
-            and event.key == pygame.K_SPACE):
-            begin_points = random_points()
-            end_points = random_points()
 
-    screen.fill((0,0,0))
-    pygame.draw.circle(screen, RED, (round(position.get_x()),
-                                     round(position.get_y())), 15)
-    position.add_to(velocity)
-    # screen.blit(label, ((screen_rect.width - label.get_rect().width) // 2,
-    #                     (screen_rect.height - 20)))
+    screen.fill((0, 0, 0))
+    for i in range(0, NUM_PARTICLES):
+        p = particles[i]
+        p.update()
+        pygame.draw.circle(screen, RED, (round(p.position.get_x()),
+                                         round(p.position.get_y())), 5)
     pygame.display.update()
 
-pygame.quit()
 
+pygame.quit()
